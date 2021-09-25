86e2e312520556d76ed4aec04d9d5ef82881a19d
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-25 10:04:02 +0900

    Update first-setup.sh

--- jp/first-setup.sh
+++ jp/first-setup.sh
@@ -4,23 +4,23 @@ function info(){
   echo $1
 }
 
-info ディスクの消耗を抑えるためにSWAPシステムを停止します
+info "ディスクの消耗を抑えるためにSWAPシステムを停止します"
 systemctl disable dphys-swapfile.service
 
-info Dockerの為の設定をします
-echo cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 >> /boot/cmdline.txt
+info "Dockerの為の設定をします"
+echo "cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1" >> /boot/cmdline.txt
 
-info piユーザーはデフォルトで設定されているユーザーのため脆弱です
-info 新しいユーザーを設定しましょう
+info "piユーザーはデフォルトで設定されているユーザーのため脆弱です"
+info "新しいユーザーを設定しましょう"
 adduser clusterpc
 usermod -aG adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio clusterpc
 
-info piユーザーを消しちゃいます!
+info "piユーザーを消しちゃいます!"
 userdel pi
 
-info 更新してあげましょう
+info "更新してます"
 apt update -y
 apt full-upgrade -y
 
-info 全ての初期プロセスが終了しました
-info 再起動してください
+info "全ての初期プロセスが終了しました"
+info "再起動してください"
