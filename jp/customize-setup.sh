49117bf21298c314f7a3d04280bf0c0b77c04009
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-25 11:38:47 +0900

    Update customize-setup.sh

--- jp/customize-setup.sh
+++ jp/customize-setup.sh
@@ -9,19 +9,19 @@ info "固定ipアドレスは$1$2のようになります"
 if [ $3 = "lan" ]; then
   info "有線LAN(LAN)を設定しています"
   cat <<EOF | tee /etc/dhcpcd.conf
-  interface eth0
-  static ip_address=$1$2/24
-  static routers=$4
-  static domain_name_servers=$5
-  EOF
+interface eth0
+static ip_address=$1$2/24
+static routers=$4
+static domain_name_servers=$5
+EOF
 else
   info "無線LAN(Wi-FI)を設定しています"
   cat <<EOF | tee /etc/dhcpcd.conf
-  interface wlan0
-  static ip_address=$1$2/24
-  static routers=$4
-  static domain_name_servers=$5
-  EOF
+interface wlan0
+static ip_address=$1$2/24
+static routers=$4
+static domain_name_servers=$5
+EOF
 fi
 
 info "まだコマンドが作成されていません..."
