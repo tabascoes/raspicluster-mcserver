c4f541f47b282e505be4525f319054308e730324
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-25 10:30:50 +0900

    Create customize-setup.sh

---
+++ jp/customize-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function info(){
+  echo -e "\e[1m$1 \e[21m"
+}
+
+info "ipアドレスを割り当てます"
+info "まだコマンドが作成されていません..."
+
+info "ユーザー名をcluster$1に変更します。"
+usermod -l cluster$1 -d /home/cluster$1 -m musterpi
