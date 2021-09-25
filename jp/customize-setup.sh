011c30243d6cc1ebf6d82142722315e8d13a3cd4
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-25 11:23:29 +0900

    Update customize-setup.sh

--- jp/customize-setup.sh
+++ jp/customize-setup.sh
@@ -5,7 +5,26 @@ function info(){
 }
 
 info "ipアドレスを割り当てます"
+info "固定ipアドレスは$1$2のようになります"
+if [ $3 = "lan" ]; then
+  info "有線LAN(LAN)を設定しています"
+  cat <<EOF | sudo tee /etc/dhcpcd.conf
+  interface eth0
+  static ip_address=$1$2/24
+  static routers=$4
+  static domain_name_servers=$5
+  EOF
+else
+  info "無線LAN(Wi-FI)を設定しています"
+  cat <<EOF | sudo tee /etc/dhcpcd.conf
+  interface wlan0
+  static ip_address=$1$2/24
+  static routers=$4
+  static domain_name_servers=$5
+  EOF
+fi
+
 info "まだコマンドが作成されていません..."
 
-info "ユーザー名をcluster$1に変更します。"
-usermod -l cluster$1 -d /home/cluster$1 -m masterpi
+info "ユーザー名をcluster$2に変更します。"
+usermod -l cluster$1 -d /home/cluster$2 -m masterpi
